b3bb4619e042dfb24f6a126baea2888fc6874a5b
Jakub Łanoszka <jakublanoszka@gmail.com>
2022-07-02 14:53:40 +0200

    pattern

--- src/regex/RegexExamples.java
+++ src/regex/RegexExamples.java
@@ -11,6 +11,8 @@ public class RegexExamples {
 
         System.out.println(matcher.matches());
 
-        Pattern emailPattern = Pattern.compile("[a-zA-Z0-9]+@[a-z]+\\.[a-z]");
+        Pattern emailPattern = Pattern.compile("[a-zA-Z0-9]+@.+\\.[a-z]+");
+        Matcher matcher1 = emailPattern.matcher("dsaqwe@gmail.com");
+        System.out.println(matcher1.matches());
     }
 }
