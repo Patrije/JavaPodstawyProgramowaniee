cbaf9cd8238ef58788bd43f4a5112b4442ad990c
Igor <igor.terentowicz@op.pl>
2022-07-03 14:01:14 +0200

    Autor

---
+++ src/Zadania/Author.java
@@ -0,0 +1,30 @@
+package Zadania;
+
+public class Author {
+
+        private String surname;
+
+        private String nationality;
+
+    public Author(String surname, String nationality) {
+        this.surname = surname;
+        this.nationality = nationality;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+}
+
