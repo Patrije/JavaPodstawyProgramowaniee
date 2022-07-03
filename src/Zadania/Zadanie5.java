ee1a6965e6f24ceeaf642ec574334df38f768502
Igor <igor.terentowicz@op.pl>
2022-07-03 09:54:59 +0200

    Prime numbers

---
+++ src/Zadania/Zadanie5.java
@@ -0,0 +1,21 @@
+package Zadania;
+
+import java.util.Scanner;
+
+public class Zadanie5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+
+        int number, digit, sum = 0;
+
+        number = scanner.nextInt();
+        while(number > 0)
+        {
+            digit = number % 10;
+            sum = sum + digit;
+            number = number / 10;
+        }
+        System.out.println("Suma cyfr liczby: "+sum);
+    }
+}
