4d750bc6b25d30c3fa6c5e65615820b1ea839d6b
Igor <igor.terentowicz@op.pl>
2022-07-03 14:01:34 +0200

    Poemat

---
+++ src/Zadania/Poem.java
@@ -0,0 +1,8 @@
+package Zadania;
+
+public class Poem {
+    public static void main(String[] args) {
+        Author creator = (new Author("Kowalski", "Polish"));
+        int stropheNumbers = (10);
+    }
+}
