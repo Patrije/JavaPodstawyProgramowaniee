308a01bc4f86cf814f614ee24c11d49eceeb6569
Igor <igor.terentowicz@op.pl>
2022-07-03 11:08:10 +0200

    Najdluzsze slowo

--- src/Zadania/Zadanie6.java
+++ src/Zadania/Zadanie6.java
@@ -1,6 +1,5 @@
 package Zadania;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Zadanie6 {
@@ -20,16 +19,13 @@ public class Zadanie6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Wpisz slowo: ");
-        ArrayList<String> list = new ArrayList<String>();
-        String str = list.toArray(new String[0]);
 
 
         while (!in.hasNext("Starczy"))
         {
-            list.add(in.next());
+            in.nextLine();
         }
-
-        String longestString = getLongestString(str);
+        String longestString = getLongestString(); //???
         System.out.format("Najdluzsze slowo to " + longestString);
     }
 }
