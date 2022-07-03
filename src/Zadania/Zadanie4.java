afc8346e193baf4ba1f75e0dd24d920c9a4df6e2
Igor <igor.terentowicz@op.pl>
2022-07-03 14:39:28 +0200

    Liczby pierwsze

--- src/Zadania/Zadanie4.java
+++ src/Zadania/Zadanie4.java
@@ -3,15 +3,25 @@ package Zadania;
 import java.util.Scanner;
 
 public class Zadanie4 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String args[]) {
+
         System.out.println("Podaj liczbe: ");
-        int liczba = scanner.nextInt();
-        int i;
-        for (i = 2; i < liczba; i++) {
-            if(i % 2 == 1){
-                System.out.println(i);
+        int limit = new Scanner(System.in).nextInt();
+
+        System.out.println(limit);
+        for (int number = 2; number <= limit; number++) {
+
+            if (isPrime(number)) {
+                System.out.println(number);
             }
         }
+    }
+    public static boolean isPrime(int number){
+        for(int i = 2; i < number; i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
