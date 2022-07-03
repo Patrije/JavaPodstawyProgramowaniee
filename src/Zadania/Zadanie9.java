f90ac76c008762fdc94ec4aa4968be3fd85cc824
Igor <igor.terentowicz@op.pl>
2022-07-03 13:53:37 +0200

    Pattern

---
+++ src/Zadania/Zadanie9.java
@@ -0,0 +1,12 @@
+package Zadania;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Zadanie9 {
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.next();
+    Pattern pattern = Pattern.compile("");
+    Matcher matcher = pattern.matcher();
+}
