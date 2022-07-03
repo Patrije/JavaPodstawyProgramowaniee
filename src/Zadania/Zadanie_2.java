0f08758c737347da448443adc9fd18850264bf72
Marcin <marcin.kdz92@gmail.com>
2022-07-03 09:15:02 +0200

    Zadanie_2

---
+++ src/Zadania/Zadanie_2.java
@@ -0,0 +1,23 @@
+package Zadania;
+
+public class Zadanie_2 {
+    public static void main(String[] args) {
+
+        countYourBMI( 176, 87.9f);
+
+    }
+
+    private static void countYourBMI(int yourHeightInCentymitres, float yourWeightInKilograms) {
+        float bmi;
+        double height = (float) yourHeightInCentymitres / 100;
+        bmi = (float) (yourWeightInKilograms / (height * height));
+
+        if(bmi >= 18.5 && bmi <= 24.9){
+            System.out.println("Your BMI is " + bmi + " and it's OK!");
+        }else if (bmi > 24.9){
+            System.out.println("Your BMI is " + bmi + " and You are overweight!");
+        }else{
+            System.out.println("Your BMI is " + bmi + " Your are underweight!");
+        }
+    }
+}
