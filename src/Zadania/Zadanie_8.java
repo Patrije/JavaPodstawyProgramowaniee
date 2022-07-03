24421aea4a9955d18a9497a325bfecfc9314b104
Marcin <marcin.kdz92@gmail.com>
2022-07-03 12:17:01 +0200

    Zadanie_8

---
+++ src/Zadania/Zadanie_8.java
@@ -0,0 +1,28 @@
+package Zadania;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Zadanie_8 {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        howManyDaysLeft();
+
+    }
+
+    private static void howManyDaysLeft() {
+        System.out.println("Enter a date of next SDA lesson [dd-MM-YYY]:");
+        String dateOfLesson = scanner.next();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        LocalDate lessonDate = LocalDate.parse(dateOfLesson, formatter);
+
+        System.out.println("You have " + Period.between(LocalDate.now(), lessonDate).getDays() + " days left to SDA lesson!");
+    }
+}
