4d9f009e51bc6d339b67e93d3b32b72b7a8f94be
Igor <igor.terentowicz@op.pl>
2022-07-03 14:22:51 +0200

    BMI

--- src/Zadania/Zadanie2.java
+++ src/Zadania/Zadanie2.java
@@ -3,20 +3,20 @@ package Zadania;
 import java.util.Scanner;
 
 public class Zadanie2 {
-    public static float main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj wage (kg): ");
         float waga = scanner.nextFloat();
+
         System.out.println("Podaj wzrost (cm): ");
         int wzrost = scanner.nextInt();
-        float wzrost2 = wzrost / 100; //wzrost w metrach
+        float wzrost2 = (float) wzrost / 100;
 
-        float wartoscBMI = waga / (wzrost2 * wzrost2);
-
+        float wartoscBMI = waga / wzrost2 * wzrost2;
+        System.out.println(wartoscBMI);
         if (wartoscBMI <= 18.5 && wartoscBMI >= 24.9) {
             System.out.println("BMI optymalne");
-        } else {
-            System.out.println("BMI nieoptymalne");
         }
-    } return //???
+        return;
+    }
 }
