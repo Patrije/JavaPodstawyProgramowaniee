ba3a6b60996f1f6e408d672b24972b629e33c9d8
Jakub Łanoszka <jakublanoszka@gmail.com>
2022-07-03 10:49:38 +0200

    Zadanie4

---
+++ src/zadania/Zadanie4.java
@@ -0,0 +1,47 @@
+package zadania;
+
+import java.util.Scanner;
+
+public class Zadanie4 {
+    static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int userNumber = loadNumber();
+        for (int i = 1; i > userNumber; ) {
+            if (userNumber <= 0) {
+                System.out.println("Wrong number! Write positive number!");
+                userNumber = loadNumber();
+                continue;
+            }
+        }
+
+        for(int i = 2 ; i<=userNumber/2; i++){
+            if(isPrimeNumber(i)){
+                System.out.println(i);
+            }
+
+        }
+
+
+
+
+
+
+        }
+        static int loadNumber () {
+            System.out.println("Podaj dodatnią liczbę: ");
+            return s.nextInt();
+        }
+
+    static boolean isPrimeNumber(int x) {
+
+        for (int i = 2; i < x/2; i++) {
+            if (x % i == 0) {
+                return false;
+
+            }
+        }
+        return true;
+    }
+    }
