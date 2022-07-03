e9157e38f5c7965d2fe5a85befb52e2d63bcd2d5
Marcin <marcin.kdz92@gmail.com>
2022-07-03 09:37:20 +0200

    Zadanie_3

---
+++ src/Zadania/Zadanie_3.java
@@ -0,0 +1,26 @@
+package Zadania;
+
+public class Zadanie_3 {
+    public static void main(String[] args) {
+
+        pifPafMethod(146);
+
+    }
+
+    private static void pifPafMethod(int enterYourNumber) {
+
+        for (int i = 0; i <= enterYourNumber; i++) {
+            if (i % 3 == 0){
+                System.out.println("PIF");
+            } else if (i % 7 == 0){
+                System.out.println("PAF");
+            } else if (i % 7 == 0 && i % 3 == 0){
+                System.out.println("PIF PAF");
+            }else{
+                System.out.println(i);
+            }
+            
+        }
+
+    }
+}
