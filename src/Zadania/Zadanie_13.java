0055ecb998510023fde0a1c189d638d51dc8e7aa
Marcin <marcin.kdz92@gmail.com>
2022-07-05 06:54:05 +0200

    Zadanie_13

---
+++ src/Zadania/Zadanie_13.java
@@ -0,0 +1,24 @@
+package Zadania;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Zadanie_13 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        isItAPostCode();
+    }
+
+    private static void isItAPostCode() {
+        System.out.println("Enter your postcode [XX-XXX]:");
+        String s1 = scanner.nextLine();
+
+        Pattern compiledPattern = Pattern.compile("[0-9]{2}-[0-9]{3}");
+        Matcher matcher = compiledPattern.matcher(s1);
+
+        System.out.println("It's postcode: " + matcher.matches());
+    }
+}
